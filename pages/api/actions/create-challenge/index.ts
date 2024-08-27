4dd62775ff01d555fe751bacdc7a58806f1401da
enigma2210 <109715661+enigma2210@users.noreply.github.com>
2024-08-28 03:29:29 +0530

    Update index.ts

--- pages/api/actions/create-challenge/index.ts
+++ pages/api/actions/create-challenge/index.ts
@@ -145,7 +145,7 @@ const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
       target: target as string,
       start_time: absoluteStartTime,
       duration: durationInSeconds,
-      wager: double((wager) * 10 ** 9)
+      wager: ((wager) * 10 ** 9)
     };
 
     console.log("Challenge JSON:", createChallengeJson);
